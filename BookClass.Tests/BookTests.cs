107886fc6f2da66c9fadf0f14ee250a3d76dee4b
marchanyanehu <dadada.marchan@gmail.com>
2023-06-30 22:17:44 +0300

    done

---
+++ BookClass.Tests/BookTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+
+namespace Test;
+[TestFixture]
+public class BookTests
+{
+    private Book? book;
+
+    [SetUp]
+    public void SetUp()
+    {
+        book = new Book("Jon Skeet", "C# in Depth", "Manning Publications", "9781617294532");
+        book.Pages = 528;
+        book.SetPrice(39.99m, "USD");
+    }
+
+    [Test]
+    public void TestConstructorProperties()
+    {
+        Assert.AreEqual("Jon Skeet", book.Author);
+        Assert.AreEqual("C# in Depth", book.Title);
+        Assert.AreEqual("Manning Publications", book.Publisher);
+        Assert.AreEqual("9781617294532", book.ISBN);
+    }
+
+    [Test]
+    public void TestPagesProperty()
+    {
+        Assert.AreEqual(528, book.Pages);
+    }
+
+    [Test]
+    public void TestPriceAndCurrencyProperties()
+    {
+        Assert.AreEqual(39.99m, book.Price);
+        Assert.AreEqual("USD", book.Currency);
+    }
+
+    [Test]
+    public void TestPublishMethod()
+    {
+        DateTime publicationDate = new DateTime(2019, 1, 1);
+        book.Publish(publicationDate);
+        Assert.AreEqual("01/01/2019", book.GetPublicationDate());
+    }
+
+    [Test]
+    public void TestToString()
+    {
+        Assert.AreEqual("C# in Depth by Jon Skeet", book.ToString());
+    }
+
+    [Test]
+    public void TestEqualsAndHashCode()
+    {
+        Book otherBook = new Book("Jon Skeet", "C# in Depth", "Manning Publications", "9781617294532");
+        Assert.IsTrue(book.Equals(otherBook));
+        Assert.AreEqual(book.GetHashCode(), otherBook.GetHashCode());
+    }
+
+    [Test]
+    public void TestCompareTo()
+    {
+        Book otherBook = new Book("Jon Skeet", "C# in Action", "Manning Publications", "9781617294521");
+        Assert.AreEqual(1, book.CompareTo(otherBook));
+    }
+
+    [Test]
+    public void TestIFormattableToString()
+    {
+        DateTime publicationDate = new DateTime(2019, 1, 1);
+        book.Publish(publicationDate);
+        string format = "D";
+        string formattedOutput = book.ToString(format, CultureInfo.InvariantCulture);
+        Assert.AreEqual("C# in Depth by Jon Skeet. 2019. Manning Publications. ISBN: 9781617294532. 528 pages. USD39.99.", formattedOutput);
+    }
+}
---
+++ BookClass/BookClass.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Globalization;
+
+public sealed class Book : IEquatable<Book>, IComparable<Book>, IComparable, IFormattable
+{
+    private bool published;
+    private DateTime datePublished;
+    private int totalPages;
+
+    public Book(string author, string title, string publisher) : this(author, title, publisher, string.Empty)
+    {
+    }
+
+    public Book(string author, string title, string publisher, string isbn)
+    {
+        Author = author;
+        Title = title;
+        Publisher = publisher;
+        ISBN = isbn;
+    }
+
+    public string Author { get; }
+    public string Title { get; }
+    public string Publisher { get; }
+    public string ISBN { get; }
+    public decimal Price { get; private set; }
+    public string ?Currency { get; private set; }
+
+    public int Pages
+    {
+        get => totalPages;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentOutOfRangeException(nameof(value));
+            totalPages = value;
+        }
+    }
+
+    public void Publish(DateTime date)
+    {
+        published = true;
+        datePublished = date;
+    }
+
+    public string GetPublicationDate()
+    {
+        return published ? datePublished.ToString("d", CultureInfo.InvariantCulture) : "Not published yet.";
+    }
+
+    public void SetPrice(decimal price, string currency)
+    {
+        Price = price;
+        Currency = currency;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} by {Author}";
+    }
+
+#pragma warning disable CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
+    public override bool Equals(object obj)
+    {
+        return Equals(obj as Book);
+    }
+
+    public bool Equals(Book other) => other != null && ISBN == other.ISBN;
+
+    public override int GetHashCode()
+    {
+        return ISBN.GetHashCode();
+    }
+
+    public int CompareTo(Book other)
+    {
+        return other == null ? 1 : Title.CompareTo(other.Title);
+    }
+
+    int IComparable.CompareTo(object obj)
+    {
+        return CompareTo(obj as Book);
+    }
+
+    public string ToString(string format, IFormatProvider formatProvider)
+    {
+        if (string.IsNullOrEmpty(format)) format = "G";
+        if (formatProvider == null) formatProvider = CultureInfo.CurrentCulture;
+
+        switch (format.ToUpperInvariant())
+        {
+            case "G":
+                return $"{Title} by {Author}";
+            case "D":
+                return $"{Title} by {Author}. {datePublished.Year}. {Publisher}. ISBN: {ISBN}. {Pages} pages. {Currency}{Price:F2}.";
+            case "P":
+                return $"{Title} by {Author}. {datePublished.Year}. {Publisher}. ISBN: {ISBN}. {Pages} pages.";
+            case "Y":
+                return $"{Title} by {Author}. {datePublished.Year}. {Publisher}. {Pages} pages.";
+            case "T":
+                return $"{Title} by {Author}. {datePublished.Year}. {Pages} pages.";
+            case "R":
+                return $"{Title} by {Author} {Currency}{Price:F2}.";
+            default:
+                throw new FormatException($"The {format} format string is not supported.");
+        }
+    }
+}
